f52a99746205726578d3ebfc37ccc3c79861dbac
Kyler Wong <wsm.kyler@gmail.com>
2024-03-08 12:31:51 +0800

    fix: friendly snipplets int

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -1,35 +1,43 @@
-return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
+local dependencies = {
+	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"rafamadriz/friendly-snippets",
 		},
-		{
-			"windwp/nvim-autopairs",
-			opts = {
-				fast_wrap = {},
-				disable_filetype = { "TelescopePrompt", "vim" },
-			},
-			config = function(_, opts)
-				require("nvim-autopairs").setup(opts)
-
-				-- setup cmp for autopairs
-				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-				require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-			end,
-		},
-
-		-- cmp sources plugins
-		{
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lua",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-		},
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
 	},
+
+	{
+		"windwp/nvim-autopairs",
+		opts = {
+			fast_wrap = {},
+			disable_filetype = { "TelescopePrompt", "vim" },
+		},
+		config = function(_, opts)
+			require("nvim-autopairs").setup(opts)
+
+			-- setup cmp for autopairs
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+
+	-- cmp sources plugins
+	{
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
+}
+
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = dependencies,
 	opts = function()
 		local cmp = require("cmp")
 
