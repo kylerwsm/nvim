9cbc467e597a34cfabb523960f44ef7c1ffd5971
Kyler Wong <wsm.kyler@gmail.com>
2024-03-09 15:33:37 +0800

    refactor: move toggleterm keymaps into plugin cfg

--- lua/core/mapping/init.lua
+++ lua/core/mapping/init.lua
@@ -12,16 +12,11 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Window right" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Window down" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Window up" })
 
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvimtree" })
+
 vim.keymap.set("n", "<leader>fm", function()
 	require("conform").format()
 end, { desc = "LSP formatting" })
-
-vim.keymap.set("n", "<C-i>", "<cmd>ToggleTerm<CR>", { desc = "Toggle floating term" }) -- toggle term in normal and terminal mode
-vim.keymap.set("t", "<Esc>", "<cmd>ToggleTerm<CR>", { desc = "Toggle floating term" }) -- toggle term in terminal mode
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvimtree" }) -- toggles nvimtree
-vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { desc = "LSP code action" }) -- show lsp code action
-vim.keymap.set("n", "<leader>ww", "<cmd>vsplit<CR>", { desc = "Vertically split current window" }) -- split current window left and right
-vim.keymap.set("n", "<leader>wx", "<C-w>q", { desc = "Close current window" }) -- close current window
 
 require("core.mapping.telescope")
 require("core.mapping.lspconfig")
--- lua/plugin/toggleterm.lua
+++ lua/plugin/toggleterm.lua
@@ -2,8 +2,9 @@ return {
 	"akinsho/toggleterm.nvim",
 	tag = "*",
 	config = function()
+		vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating term" }) -- toggle term in normal and terminal mode
+
 		require("toggleterm").setup({
-			direction = "float",
 			auto_scroll = true, -- automatically scroll to the bottom on terminal output
 		})
 	end,
