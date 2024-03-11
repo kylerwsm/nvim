e1ccb5ad347e9827da41e7e152a75fe6705adc7d
Kyler Wong <wsm.kyler@gmail.com>
2024-03-11 22:58:50 +0800

    fix: toggleterm lazy plugin cfg

--- lua/core/plugin/toggleterm.lua
+++ lua/core/plugin/toggleterm.lua
@@ -1,11 +1,11 @@
 return {
-	"akinsho/toggleterm.nvim",
-	tag = "*",
-	config = function()
-		vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating term" }) -- toggle term in normal and terminal mode
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating term" }) -- toggle term in normal and terminal mode
 
-		require("toggleterm").setup({
-			auto_scroll = true, -- automatically scroll to the bottom on terminal output
-		})
-	end,
+    require("toggleterm").setup({
+      auto_scroll = true, -- automatically scroll to the bottom on terminal output
+    })
+  end,
 }
