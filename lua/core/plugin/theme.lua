ec76a74766a806913f30ac76ba4475b6a1ae7588
Kyler Wong <wsm.kyler@gmail.com>
2024-03-12 10:30:22 +0800

    feat: use catppuccin latte theme

--- lua/core/plugin/theme.lua
+++ lua/core/plugin/theme.lua
@@ -1,25 +1,28 @@
-local catppuccin = {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    flavour = "mocha",
+local theme_table = {
+  catppuccin = {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "latte",
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
-  config = function(_, opts)
-    require("catppuccin").setup(opts)
-    vim.cmd.colorscheme("catppuccin")
-  end,
+  github = {
+    "projekt0n/github-nvim-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('github-theme').setup({})
+      vim.cmd('colorscheme github_light_default')
+    end,
+  },
 }
 
-local github = {
-  "projekt0n/github-nvim-theme",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('github-theme').setup({})
-    vim.cmd('colorscheme github_light')
-  end,
-}
+local selected_theme = "catppuccin"
 
-return github
+return theme_table[selected_theme]
