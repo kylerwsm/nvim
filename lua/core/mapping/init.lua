7eaeac2a59cba11e066e1db9dcbf1a3f02398ddf
Kyler Wong <wsm.kyler@gmail.com>
2024-03-12 22:13:05 +0800

    feat: mappings for buf management

--- lua/core/mapping/init.lua
+++ lua/core/mapping/init.lua
@@ -1,22 +1,29 @@
+local map = vim.keymap.set
+
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "Clear highlights" })
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "Clear highlights" })
 
-vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move left" })
-vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move right" })
-vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move down" })
-vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Move up" })
+map("i", "<C-h>", "<Left>", { desc = "Move left" })
+map("i", "<C-l>", "<Right>", { desc = "Move right" })
+map("i", "<C-j>", "<Down>", { desc = "Move down" })
+map("i", "<C-k>", "<Up>", { desc = "Move up" })
 
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Window left" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Window right" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Window down" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Window up" })
+map("n", "<C-h>", "<C-w>h", { desc = "Window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "Window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "Window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Window up" })
 
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvimtree" })
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvimtree" })
 
-vim.keymap.set("n", "<leader>fm", function()
+map("n", "<leader>fm", function()
 	require("conform").format()
 end, { desc = "LSP formatting" })
+
+map("n", "<leader>b", "<cmd>enew<CR>", { desc = "Buffer New" })
+map("n", "<tab>", "<cmd>bnext<CR>", { desc = "Buffer Goto next" })
+map("n", "<S-tab>", "<cmd>bprevious<CR>", { desc = "Buffer Goto prev" })
+map("n", "<leader>x", "<cmd>bdelete<CR>", { desc = "Buffer Close" })
 
 require("core.mapping.telescope")
 require("core.mapping.lspconfig")
