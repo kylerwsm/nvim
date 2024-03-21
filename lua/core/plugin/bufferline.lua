bc1eb28fbb81af9b47a8bd577dc4e71b2daaa0cc
Kyler Wong <wsm.kyler@gmail.com>
2024-03-21 21:47:29 +0800

    feat: bufferline changes

--- lua/core/plugin/bufferline.lua
+++ lua/core/plugin/bufferline.lua
@@ -1,9 +1,20 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    vim.opt.termguicolors = true
-    require("bufferline").setup({})
-  end
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "Explorer",
+						highlight = "Directory",
+					},
+				},
+			},
+		})
+	end,
 }
