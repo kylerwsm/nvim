b862be4defd228f0dc4703e6968aaa076fc53a3d
Kyler Wong <wsm.kyler@gmail.com>
2024-03-25 15:30:54 +0800

    feat: esc to normal mode in toggleterm float

--- lua/core/plugin/toggleterm.lua
+++ lua/core/plugin/toggleterm.lua
@@ -1,25 +1,36 @@
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	config = function()
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  keys = {
+    {
+      "<C-t>",
+      [[<cmd>ToggleTerm direction=float<CR>]],
+      mode = { "n", "t" },
+      desc = "Toggle floating term",
+    },
+    {
+      "<C-g>",
+      [[<cmd>lua _lazygit_toggle()<CR>]],
+      mode = { "n", "t" },
+      desc = "Toggle lazygit",
+    },
+    {
+      "<Esc>",
+      [[<C-\><C-n>]],
+      mode = "t",
+      desc = "Exit terminal mode",
+    },
+  },
+  config = function()
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
 
-		function _lazygit_toggle()
-			lazygit:toggle()
-		end
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
 
-		vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating term" })
-
-		vim.api.nvim_set_keymap(
-			"n",
-			"<C-g>",
-			"<cmd>lua _lazygit_toggle()<CR>",
-			{ desc = "Toggle lazygit", noremap = true, silent = true }
-		)
-
-		require("toggleterm").setup({
-			auto_scroll = true, -- automatically scroll to the bottom on terminal output
-		})
-	end,
+    require("toggleterm").setup({
+      auto_scroll = true, -- automatically scroll to the bottom on terminal output
+    })
+  end,
 }
