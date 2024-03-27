cadb71a6efa209c6d54df2aa2da87d2031a18d7f
Kyler Wong <wsm.kyler@gmail.com>
2024-03-27 13:39:15 +0800

    feat: improve keybindings for gitsigns

--- lua/core/plugin/gitsigns.lua
+++ lua/core/plugin/gitsigns.lua
@@ -1,8 +1,33 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup({
-      current_line_blame = true,
-    })
-  end
+	"lewis6991/gitsigns.nvim",
+	keys = {
+		{
+			"<leader>gs",
+			[[<cmd>lua require("gitsigns").stage_hunk()<CR>]],
+			desc = "Stage hunk",
+		},
+		{
+			"<leader>gu",
+			[[<cmd>lua require("gitsigns").undo_stage_hunk()<CR>]],
+			desc = "Undo stage hunk",
+		},
+		{
+			"<leader>gr",
+			[[<cmd>lua require("gitsigns").reset_hunk()<CR>]],
+			desc = "Reset hunk",
+		},
+		{
+			"<leader>gp",
+			[[<cmd>lua require("gitsigns").preview_hunk()<CR>]],
+			desc = "Preview hunk",
+		},
+		{
+			"<leader>gb",
+			[[<cmd>lua require("gitsigns").blame_line()<CR>]],
+			desc = "Blame line",
+		},
+	},
+	config = function()
+		require("gitsigns").setup({})
+	end,
 }
