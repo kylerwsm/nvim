75deb8cefcb30e427b3773df643ffa39af85b20d
Kyler Wong <wsm.kyler@gmail.com>
2024-03-27 21:35:56 +0800

    feat: trigger grabbyright on go file format

--- lua/core/plugin/conform.lua
+++ lua/core/plugin/conform.lua
@@ -1,24 +1,30 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>fm",
-			function()
-				require("conform").format()
-			end,
-			desc = "LSP formatting",
-		},
-	},
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			javascript = { { "prettierd", "prettier" } },
-			go = { "goimports", "gofmt" },
-			sh = { "shfmt" },
-		},
-		format_on_save = { timeout_ms = 500, lsp_fallback = true },
-	},
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>fm",
+      function()
+        require("conform").format()
+      end,
+      desc = "LSP formatting",
+    },
+  },
+  opts = {
+    formatters = {
+      grabbyright = {
+        command = "grabbyright",
+        args = { "$FILENAME" },
+      },
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { { "prettierd", "prettier" } },
+      go = { "goimports", "gofmt" },
+      sh = { "shfmt" },
+    },
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+  },
 }
