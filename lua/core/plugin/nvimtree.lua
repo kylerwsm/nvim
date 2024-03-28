5e3efff962cd0bb82d90cf4f2e723b7e363bb6a2
Kyler Wong <wsm.kyler@gmail.com>
2024-03-28 17:59:29 +0800

    feat: enable git indicator in nvimtree

--- lua/core/plugin/nvimtree.lua
+++ lua/core/plugin/nvimtree.lua
@@ -1,80 +1,80 @@
 local opts = {
-	update_focused_file = {
-		enable = true,
-	},
+  update_focused_file = {
+    enable = true,
+  },
 
-	view = {
-		adaptive_size = true,
-		float = {
-			enable = false,
-		},
-	},
+  view = {
+    adaptive_size = true,
+    float = {
+      enable = false,
+    },
+  },
 
-	renderer = {
-		root_folder_label = false,
-		highlight_git = false,
-		highlight_opened_files = "none",
+  renderer = {
+    root_folder_label = false,
+    highlight_git = false,
+    highlight_opened_files = "none",
 
-		indent_markers = {
-			enable = false,
-		},
+    indent_markers = {
+      enable = false,
+    },
 
-		icons = {
-			show = {
-				file = true,
-				folder = true,
-				folder_arrow = true,
-				git = false,
-			},
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
 
-			glyphs = {
-				default = "󰈚",
-				symlink = "",
-				folder = {
-					default = "",
-					empty = "",
-					empty_open = "",
-					open = "",
-					symlink = "",
-					symlink_open = "",
-					arrow_open = "",
-					arrow_closed = "",
-				},
-				git = {
-					unstaged = "✗",
-					staged = "✓",
-					unmerged = "",
-					renamed = "➜",
-					untracked = "★",
-					deleted = "",
-					ignored = "◌",
-				},
-			},
-		},
-	},
+      glyphs = {
+        default = "󰈚",
+        symlink = "",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+          arrow_open = "",
+          arrow_closed = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
+  },
 }
 
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	keys = {
-		{
-			"<leader>e",
-			"<cmd>NvimTreeToggle<CR>",
-			desc = "Toggle nvimtree",
-		},
-	},
-	config = function()
-		-- Automatically open file upon creation
-		local api = require("nvim-tree.api")
-		api.events.subscribe(api.events.Event.FileCreated, function(file)
-			vim.cmd("edit " .. file.fname)
-		end)
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    {
+      "<leader>e",
+      "<cmd>NvimTreeToggle<CR>",
+      desc = "Toggle nvimtree",
+    },
+  },
+  config = function()
+    -- Automatically open file upon creation
+    local api = require("nvim-tree.api")
+    api.events.subscribe(api.events.Event.FileCreated, function(file)
+      vim.cmd("edit " .. file.fname)
+    end)
 
-		require("nvim-tree").setup(opts)
-	end,
+    require("nvim-tree").setup(opts)
+  end,
 }
